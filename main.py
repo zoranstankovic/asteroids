76647e68847ab80a55e59d8345f631e8c7868073
zoranstankovic <6569042+zoranstankovic@users.noreply.github.com>
2025-02-27 10:08:30 +0100

    Change to use screen instead of pygame.Surface

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color="black")
+
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
